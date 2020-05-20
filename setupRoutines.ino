91cdf6efa5a7357e5b312b3825e0c5a430e079b5
PJ <33669966+PhillipJacobsen@users.noreply.github.com>
2020-05-19 21:24:46 -0600

    Update setupRoutines.ino

--- setupRoutines.ino
+++ setupRoutines.ino
@@ -43,8 +43,8 @@ void setup()
 #endif
   WiFiMQTTclient.enableLastWillMessage("scooter/TRXA2NUACckkYwWnS9JRkATQA453ukAcD1/lastwill", "{\"status\":\"Broken\"}");  // You can activate the retain flag by setting the third parameter to true
 
- // WiFiMQTTclient.enableMQTTConnect();               //experimental - requires modified MQTT library
- // WiFiMQTTclient.enableMACaddress_for_ClientName(); //experimental - requires modified MQTT library
+  WiFiMQTTclient.enableMQTTConnect();               //experimental - requires modified MQTT library
+  WiFiMQTTclient.enableMACaddress_for_ClientName(); //experimental - requires modified MQTT library
 
   //--------------------------------------------
   //configure the 2.4" TFT display and the touchscreen controller
